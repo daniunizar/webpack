e4d11ac54354b73b4fcc77671fb74b6b67be399e
Dani <daniunizar@gmail.com>
2021-05-24 16:30:03 +0200

    Conseguida la inserción de imágenes de la forma que no queremos, desde index.js. La queremos desde index.html

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import _ from 'lodash';
 import './style.css';
+import img_lisa from './img/lisa.jpg';
+
+function component() {
+    const element = document.createElement('div');
+ 
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+ 
+   // Add the image to our existing div.
+   const myIcon = new Image();
+   myIcon.src = img_lisa;
+ 
+   element.appendChild(myIcon);
+ 
+    return element;
+  }
+  document.body.appendChild(component());  
+
