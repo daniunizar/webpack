c260b5f9890ac433f4c8cb59ca54f492b199f648
Dani <daniunizar@gmail.com>
2021-05-25 10:27:52 +0200

    Eliminado warning con definición de modo desarrollo y producción en package.json. Añadido 'Autorefresco' con 'watch' en modo desarrollo, que evita realizar todo el rato npm run dev

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function component() {
     const element = document.createElement('div');
  
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['HEllo', 'webpack'], ' ');
     element.classList.add('hello');
  
    // Add the image to our existing div.
